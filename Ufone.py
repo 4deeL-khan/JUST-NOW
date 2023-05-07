429f6561328d6a68f4ce5bdd554ca274a431fdfd
4deeL-khan <110104681+4deeL-khan@users.noreply.github.com>
2023-05-08 02:41:39 +0500

    Create Ufone.py

---
+++ Ufone.py
@@ -0,0 +1,44 @@
+        }
+        ._hj-widget-container ._hj-3C0a0__styles__inputField._hj-TaPvM__styles__dark:hover {
+            background-color: transparent !important;
+        }
+        ._hj-widget-container ._hj-3C0a0__styles__inputField:focus {
+            background-color: transparent !important;
+        }
+        ._hj-widget-container ._hj-2MC3g__styles__closeEndedOptionCommentBox textarea:focus {
+            background-color: transparent !important;
+        }
+        div[aria-label="Survey"] textarea:focus {
+            background: transparent !important;
+        }
+        /*Survey CSS End */
+        
+        .container_scale {
+            display: none !important;
+        }
+        div.vc_custom_1593870238087
+        {
+            display: block !important;
+        }
+        
+        .spu-box, .spu-box * {
+          box-sizing: border-box;
+        }
+        .spu-box {
+          display: none;
+        }
+        .pop-web{
+           display:block;
+        }
+        .pop-mob{
+           display:none;
+        }
+        #spu-12281 .spu-close {
+          font-size: 30px;
+          color: #eb7c08;
+          text-shadow: 0 1px 0;
+        }
+        .spu-icon  {
+          display: none;
+        }
+        .spu-close.top_right {
